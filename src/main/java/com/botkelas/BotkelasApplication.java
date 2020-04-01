059676bfdd40a1edd0e0f3f65f85a822304eba3f
Rahman Nurhidayat <rahmannurhidayat022@gmail.com>
2020-04-01 10:47:21 +0700

    initial commit

---
+++ src/main/java/com/botkelas/BotkelasApplication.java
@@ -0,0 +1,13 @@
+package com.botkelas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotkelasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotkelasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/botkelas/BotkelasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.botkelas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotkelasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
